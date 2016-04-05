d31077a745e74874abbf4e7b0c85cce86e40040c
JBerkvens <Account@Moridrin.com>
2016-04-05 10:48:00 -0400

    MP-SSV- folder base renamed to mp-ssv- folder base.

--- mp-ssv-frontend-members.php
+++ mp-ssv-frontend-members.php
@@ -23,8 +23,8 @@ require_once 'filter_content.php';
 add_filter( 'the_content', 'mp_ssv_filter_frontend_members_content', 100);
 
 function mp_ssv_register_mp_ssv_frontend_members() {
-	if (!is_plugin_active('MP-SSV-General/mp-ssv-general.php')) {
-		wp_die('Sorry, but this plugin requires <a href="http://studentensurvival.com/plugins/mp-ssv-general">SSV Gemeral</a> to be installed and active. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
+	if (!is_plugin_active('mp-ssv-general/mp-ssv-general.php')) {
+		wp_die('Sorry, but this plugin requires <a href="http://studentensurvival.com/plugins/mp-ssv-general">SSV General</a> to be installed and active. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
 	}
 	/* Database */
 	global $wpdb;
