9e500d1d2abe797c0f4d66535417a674d586c0ad
jeroen <Jeroen@QForma.nl>
2017-02-22 12:52:22 +0100

    Export Working

--- ssv-users.php
+++ ssv-users.php
@@ -331,3 +331,90 @@ function mp_ssv_users_set_profile_page_title($title, $id)
 
 add_filter('the_title', 'mp_ssv_users_set_profile_page_title', 20, 2);
 #endregion
+
+#region Export
+function mp_ssv_users_generate_data() {
+    if (SSV_General::isValidPOST(SSV_Users::ADMIN_REFERER_EXPORT)) {
+        if (ini_get('safe_mode') == false) {
+            set_time_limit(0);
+            ini_set('memory_limit', '2048M');
+        }
+        $fields = explode(',', SSV_General::sanitize($_POST['field_names']));
+        $users  = get_users();
+        if (!$users) {
+            return; //TODO Add Message;
+        }
+
+        $filename = get_bloginfo('name') . ' users ' . date('Y-m-d H:i:s');
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename=' . $filename . '.csv');
+        header('Content-Type: text/csv; charset=' . get_option('blog_charset'), true);
+
+
+        // build the document headers ##
+        $headers = array();
+        foreach ($fields as $key => $field) {
+            $headers[] = '"' . $field . '"';
+        }
+        ob_end_flush();
+
+        // get the value in bytes allocated for Memory via php.ini ##
+        // @link http://wordpress.org/support/topic/how-to-exporting-a-lot-of-data-out-of-memory-issue
+        $memory_limit = ini_get('memory_limit');
+        $memory_limit = trim( $memory_limit );
+        $last = strtolower($memory_limit[strlen($memory_limit)-1]);
+        switch( $last ) {
+            case 'g':
+                $memory_limit *= 1024;
+                break;
+            case 'm':
+                $memory_limit *= 1024;
+                break;
+            case 'k':
+                $memory_limit *= 1024;
+                break;
+        }
+        $memory_limit = $memory_limit * .75;
+
+        // we need to disable caching while exporting because we export so much data that it could blow the memory cache
+        // if we can't override the cache here, we'll have to clear it later...
+        if (function_exists('override_function')) {
+            override_function('wp_cache_add', '$key, $data, $group="", $expire=0', '');
+            override_function('wp_cache_set', '$key, $data, $group="", $expire=0', '');
+            override_function('wp_cache_replace', '$key, $data, $group="", $expire=0', '');
+            override_function('wp_cache_add_non_persistent_groups', '$key, $data, $group="", $expire=0', '');
+        } elseif (function_exists('runkit_function_redefine')) {
+            runkit_function_redefine('wp_cache_add', '$key, $data, $group="", $expire=0', '');
+            runkit_function_redefine('wp_cache_set', '$key, $data, $group="", $expire=0', '');
+            runkit_function_redefine('wp_cache_replace', '$key, $data, $group="", $expire=0', '');
+            runkit_function_redefine('wp_cache_add_non_persistent_groups', '$key, $data, $group="", $expire=0', '');
+        }
+        echo implode(',', $headers) . "\n";
+        foreach ($users as $user) {
+            // check if we're hitting any Memory limits, if so flush them out ##
+            // per http://wordpress.org/support/topic/how-to-exporting-a-lot-of-data-out-of-memory-issue?replies=2
+            if (memory_get_usage(true) > $memory_limit) {
+                wp_cache_flush();
+            }
+
+            $data     = array();
+            $userMeta = (array)get_user_meta($user->ID);
+            foreach ($fields as $field) {
+                if (isset($userMeta[$field])) {
+                    $value = $userMeta[$field][0];
+                } else {
+                    $value = isset($user->{$field}) ? $user->{$field} : null;
+                }
+                if (is_array($value)) {
+                    $value = implode(';', $value);
+                }
+                $value  = SSV_General::sanitize($value);
+                $data[] = '"' . str_replace('"', '""', $value) . '"';
+            }
+            echo implode(',', $data) . "\n";
+        }
+        exit;
+    }
+}
+add_action('admin_init', 'mp_ssv_users_generate_data');
+#endregion
