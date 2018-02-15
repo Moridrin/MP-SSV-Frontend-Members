75f52e16264272d054679e8bb85fbf049de51d20
Justin <jszanto@users.noreply.github.com>
2018-02-15 23:00:55 +0100

    A few fixes

--- custom-post-type/atms-registration-fields.php
+++ custom-post-type/atms-registration-fields.php
@@ -8,13 +8,36 @@ class ATMSRegistrationFields
 {
     public function render($atts, $content)
     {
+        $success = false;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            return ATMS_API::post('register-member', $_POST);
+            $result = ATMS_API::post('register-member', $_POST, true);
+            $success = !($result['status'] == 200);
+            $form = $result['body'];
+        } else {
+            $form = ATMS_API::get('registration-form');
         }
-        $html = '<form method="POST">';
-        $html .= ATMS_API::get('registration-form');
-        $html .= '</form>';
-        return $html;
+        ob_start();
+        if ($success):
+        ?>
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title"><?= isset($atts['success-title']) ? $atts['success-title'] : 'success-title' ?></span>
+                    <p><?= isset($atts['success-desc']) ? $atts['success-desc'] : 'success-desc' ?></p>
+                </div>
+            </div>
+        <?php else: ?>
+        <div class="card">
+            <div class="card-content">
+                <span class="card-title"><?= isset($atts['title']) ? $atts['title'] : 'title' ?></span>
+                <p><?= isset($atts['form-desc']) ? $atts['form-desc'] : 'form-desc' ?></p>
+            </div>
+        </div>
+        <form method="POST">
+            <?= $form ?>
+        </form>
+        <?php
+        endif;
+        return ob_get_clean();
     }
 }
 
